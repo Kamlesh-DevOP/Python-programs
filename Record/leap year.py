144dad3eed1329042fb77c4da17b88a6b2eb6059
Kamlesh <tjkamlesh@gmail.com>
2022-09-13 18:05:47 +0530

    final

--- Record/leap year.py
+++ Record/leap year.py
@@ -4,6 +4,6 @@ if (year%4==0):
         print('It is a leap year')
     elif (year%400==0):
         print('It is a leap year')
-    else: print('It is not a leap year')
+    else: print('Not a leap year')
 else:
     print('It is not a leap year')
