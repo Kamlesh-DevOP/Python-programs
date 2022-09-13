57ab38f6f567f60f32aeadf70a89601fc836e149
Kamlesh <tjkamlesh@gmail.com>
2022-09-13 18:58:55 +0530

    final

--- All files/pattern20.py
+++ All files/pattern20.py
@@ -4,8 +4,12 @@
 56789'''
 n=int(input('Enter a number: '))
 m=1
-for i in range(1,n+1):
-    for k in range(1,2*i):
+i=1
+while i<=n:
+    k=1
+    while k<2*i:
         print(m,end=' ')
         m+=1
+        k+=1
+    i+=1
     print()
