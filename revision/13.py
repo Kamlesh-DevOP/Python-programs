45f2c34bc99f7b5bdc998d3a924aaab298ffde1b
Kamlesh <98231570+Kamlesh-DevOP@users.noreply.github.com>
2022-10-16 13:14:27 +0530

    Delete revision directory

--- revision/13.py
+++
@@ -1,5 +0,0 @@
-for i in range(4):
-    for k in range(i+1):
-        s=(4-k)*2
-        print(s,end=' ')
-    print()
--- revision/pali.py
+++
@@ -1,3 +0,0 @@
-for i in range(1000,5001):
-    if i==int(str(i)[::-1]):
-        print(i,end=' ')
