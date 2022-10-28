ea3afd6ace076984f6a545ececed16d92a7207d3
Kamlesh <tjkamlesh@gmail.com>
2022-10-29 00:28:22 +0530

    common elements deleted. number work remains

--- All files/FLAMES.py
+++ All files/FLAMES.py
@@ -1,6 +1,51 @@
-n1=input('Enter your name: ')
-n2=input('Enter your crush\'s name: ')
-count=0
+name1=input('Enter your name: ')
+name2=input('Enter your crush\'s name: ')
+lst1=list(name1)
+lst2=list(name2)
+len1=len(lst1)
+len2=len(lst2)
+i=0
+while i<len1:
+    j=0
+    while j<len2:
+        if lst1[i]==lst2[j]:
+            del lst1[i]
+            del lst2[j]
+            i-=1
+            len1-=1
+            len2-=1
+        j+=1
+    i+=1
+num=len(lst1+lst2)
+flames=list('FLAMES')
+num=num%6
+k=0
+cou=0
+length=len(flames)
+while k<length:
+    cou+=1
+    if cou==num:
+        del flames[cou-1]
+    k+=1
+    length-=1
+#incomplete
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 l=[]
 name=n1+n2
 for i in name:
@@ -22,4 +67,4 @@ elif count%6==5:
     print('Enemies''\nErr... Enemies.. Everybody wants to be my enemy...')
 else:
     print('Sister''\nSister bro are best couples! ಥ_ಥ')
-#incomplete
+#incomplete'''
