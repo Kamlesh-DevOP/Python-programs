c2f42ac8c1faaa56851a93d98bcb52f31eb95297
Kamlesh <tjkamlesh@gmail.com>
2022-11-01 20:46:30 +0530

    final

--- All files/5x5 mean of diag.py
+++ All files/5x5 mean of diag.py
@@ -1,11 +1,13 @@
 #find mean of diagonal elements excluding centre element
 l=eval(input('Enter list: '))
-c=0
+sum=0
+count=0
 for i in range(5):
     for j in range(5):
         if ((i==0 or i==4) and (j==0 or j==4)) or ((i==1 or i==3) and (j==1 or j==3)):
             print(l[i][j],end=' ')
-            c+=l[i][j]
+            sum+=l[i][j]
+            count+=1
         else: print(' ', end=' ')
     print()
-print('mean: ',c/8)
+print('mean: ',sum/count)
