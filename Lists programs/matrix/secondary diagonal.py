fb390305f286fa80f27b3aa0672b0fe90f6321a7
Kamlesh <tjkamlesh@gmail.com>
2022-11-01 20:32:17 +0530

    final

--- Lists programs/matrix/secondary diagonal.py
+++ Lists programs/matrix/secondary diagonal.py
@@ -2,7 +2,7 @@
 l=eval(input('Enter list: '))
 for i in range(len(l)):
     for j in range(len(l)-1,-1,-1):
-        if i+j==2:
+        if i+j==len(l)-1:
             print(l[i][j], end=' ')
         else: print('  ', end=' ')
     print()
