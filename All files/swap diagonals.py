4a4fe35253c58ec44c7c3a0376df41bf5fc92cb3
Kamlesh <tjkamlesh@gmail.com>
2022-11-12 17:30:52 +0530

    final

---
+++ All files/swap diagonals.py
@@ -0,0 +1,7 @@
+l=eval(input('Enter matrix: '))
+for i in range(len(l)):
+    for j in range(len(l)):
+        k=len(l)-1
+        if i==j:
+            l[i][j],l[i][k-j]=l[i][k-j],l[i][j]
+        k-=1
