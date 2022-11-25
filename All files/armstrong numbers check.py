32dac13bf832869a107a247c0c9c02aa1585b9d7
Kamlesh <tjkamlesh@gmail.com>
2022-11-25 20:02:21 +0530

    final

--- All files/armstrong numbers check.py
+++ All files/armstrong numbers check.py
@@ -1,7 +1,9 @@
 n=int(input('Enter any number: '))
 s=0
+p=len(str(n))
 for i in str(n):
-    p=len(str(n))
     s+=int(i)**p
-    if s==n:
-        print(n,'is an armstrong number!')
+if s==n:
+    print(n,'is an armstrong number!')
+else:
+    print(n,'is not an armstrong number')
