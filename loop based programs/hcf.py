423c9060182881b8bb51dcc49d80a052b7906dc1
Kamlesh <tjkamlesh@gmail.com>
2022-11-30 16:19:42 +0530

    final

--- loop based programs/hcf.py
+++ loop based programs/hcf.py
@@ -1,11 +1,11 @@
 #from Shaashu
 n=int(input('Enter number 1: '))
 m=int(input('Enter number 2: '))
-if n>m:
-    p=m
+if n<m:
+    small=n
 else:
-    p=n
-for i in range(1,(p)+1):
-    if n%i==0  and m%i==0: #checks if both are divisible by a number from 1 to smaller number
-        hcf=i 
+    small=m
+for i in range(1,(small)+1):
+    if n%i==0  and m%i==0:
+        hcf=i
 print(hcf)
