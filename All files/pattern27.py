1ba6c5ed24621f0e63d2ae57857b1538186e9362
Kamlesh <tjkamlesh@gmail.com>
2022-12-04 03:18:38 +0530

    final

---
+++ All files/pattern27.py
@@ -0,0 +1,13 @@
+'''
+1-1
+12-3
+123-6
+'''
+n=int(input('Enter n: '))
+for i in range(1,n+1):
+    sum=0
+    for j in range(1,i+1):
+        sum+=j
+        print(j,end='')
+    print('-',sum,sep='')
+    print()
