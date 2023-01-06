3642b5d469c2378138114f8735f5abae813bcc62
Kamlesh <tjkamlesh@gmail.com>
2023-01-06 17:43:55 +0530

    final

---
+++ Leetcode questions/Plus one.py
@@ -0,0 +1,11 @@
+digits=eval(input('Enter list of digits: '))
+i=len(digits)-1
+while digits[i]==9 and i>=0:
+    digits[i]=0
+    i-=1
+if i==-1:
+    digits.insert(0,1)
+else:
+    digits[-1]+=1
+print(digits)
+#wow
