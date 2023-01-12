5387b03e1367317b676b8caf97e1059e44217ec7
Kamlesh <tjkamlesh@gmail.com>
2023-01-13 00:05:10 +0530

    final

---
+++ Patterns/star pattern5.py
@@ -0,0 +1,15 @@
+for i in range(0,5):
+    for j in range(0,i):
+        print(' ',end='')
+    for k in range(i+1,6):
+        print('*',end='')
+    for l in range(i,4):
+        print('*',end='')
+    print()
+'''  
+*********
+ *******
+  *****
+   ***
+    *
+'''
