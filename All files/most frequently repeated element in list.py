c702e3a61c665aa4005bb0e5dd279fd13f5df83a
Kamlesh <tjkamlesh@gmail.com>
2023-02-18 19:07:18 +0530

    final

---
+++ All files/most frequently repeated element in list.py
@@ -0,0 +1,24 @@
+'''
+Write a program which accepts a list of integers and stores the
+element(s) which is(are) present maximum number of times in a new
+list and prints the resultant list /error message .
+Eg1. If the original list is [ 4,3,5,4,1,3,6,4,4,3,2,4]
+Then the new list will be [4].
+Eg2. If the original list is [4,3,4,5,6,3,7]
+Then the new list will be [4,3].
+Eg3. If the original list is [3,4,5,6,7]
+The new list will be [3,4,5,6,7]
+'''
+
+l=eval(input('enter list: '))
+a=[]
+max_count=0
+for i in l:
+    count=l.count(i)
+    if count>max_count:
+        max_count=count
+for j in l:
+    if l.count(j)==max_count:
+        if j not in a:
+            a.append(j)
+print(a)
