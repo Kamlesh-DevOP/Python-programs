9d86d07f949bc7fccdd6ef286f5b77fde2b94507
Kamlesh <tjkamlesh@gmail.com>
2023-02-22 20:29:08 +0530

    final

--- All files/common prefix.py
+++ All files/common prefix.py
@@ -12,23 +12,30 @@ Explanation: There is no common prefix among the input strings.
 '''
 arr=eval(input('Enter array of strings: '))
 minelement=arr[0]
+arr.remove(minelement)
 b=True
+for i in arr:
+    if i==minelement:
+        print(minelement)
+        break
+    else:
+        break
+
+
 for word in arr:
     if len(word)<len(minelement):
         minelement=word
 minelementlength=len(minelement)
-arr.remove(minelement)
-
 for i in range(1,minelementlength+1):
     for word in arr:
         check=minelement
         if word[0:i]!=check[0:i]:
-               
+            print(minelement[:i-1])
             b=False
             break
     if not b:
         break
-print(minelement[:i-1])
+
 
 #guha's question in annual practicals. thank god i didnt get this... literally went through a breakdown and 2 depressing days to finally 
 #complete this
