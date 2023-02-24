83a38768b11caa9c364a2e340c0afd9f3c6a20ac
Kamlesh <tjkamlesh@gmail.com>
2023-02-24 17:23:56 +0530

    added comment

--- Leetcode questions/202. Happy Number.py
+++ Leetcode questions/202. Happy Number.py
@@ -1,6 +1,7 @@
 s=int(input('Enter number: '))
-l=[]
-while s!=1:
+l=set() #i used set becuase sets are unordered and have O(1) time complexity while list has O(n). also, sets are the best to use if searching something is more important
+# i got 11ms more speed once i used set!!
+while s!=1: 
     s=str(s)
     sum=0
     for i in s:
@@ -9,5 +10,6 @@ while s!=1:
 
     if s in l:
         print('False')
-    l+=[s]
+    l.add(s)
 print('True')
+ ##jayash orz orz
